b5bedab5092780a32b6306932ce8c362ba8a7031
nasramal <amalradwan.nasr@gmail.com>
2024-04-02 22:30:11 +0300

    backend

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,11 +1,10 @@
 const express = require ("express")
-//, getOrderByUserId, DeleteOrderbyID
 const {createOrder,getOrderByUserId,DeleteOrderbyID} = require("../controllers/order")
 const authentication = require("../middleware/authentication")
 const authorization = require("../middleware/authorization")
 const orderRouter = express.Router();
 orderRouter.post("/",authentication,authorization ("CREATE_user"),createOrder);
-orderRouter.get("/:id",getOrderByUserId);
+orderRouter.get("/:id",authentication,authorization ("get_user"),getOrderByUserId);
 orderRouter.delete("/delete/:id",DeleteOrderbyID)
 
 module.exports = orderRouter
